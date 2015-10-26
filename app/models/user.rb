a2d7edc41db6d454cc0f9e26765af48a75f783e4
Dmitry Maksyoma <ledestin@gmail.com>
2015-10-26 23:51:12 +1300

    Add ability to add user and their manager

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,21 @@ class User < ActiveRecord::Base
     email, first, last = *user
     User.create! email_address: email, first_name: first, last_name: last
   end
+
+  def self.import_users users
+    unfinished_users = {}
+
+    users.each do |user|
+      u = import_user user
+
+      _email, _first, _last, manager_email = *user
+      unfinished_users[u] = manager_email if manager_email
+    end
+
+    unfinished_users.each do |user, manager_email|
+      next unless manager = User.find_by_email_address(manager_email)
+
+      user.update_attribute :parent_id, manager.id
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 describe User do
   let(:user_without_manager) { ['a@example.com', 'John', 'Doe'] }
+  let(:manager) { ['manager@example.com', 'Mike', 'Bolton'] }
   let(:user_with_manager) do
-    ['a@example.com', 'John', 'Doe', 'manager@example.com']
+    ['user@example.com', 'John', 'Doe', manager.first]
   end
 
   describe '.import_user' do
@@ -20,11 +21,23 @@ describe User do
     it 'imports user that has manager' do
       User.import_user user_with_manager
 
-      user = User.find_by_email_address('a@example.com')
-      expect(user.email_address).to eq 'a@example.com'
+      user = User.find_by_email_address('user@example.com')
+      expect(user.email_address).to eq 'user@example.com'
       expect(user.first_name).to eq 'John'
       expect(user.last_name).to eq 'Doe'
       expect(user.parent_id).to be_nil
+    end
+  end
+
+  describe '.import_users' do
+    it 'imports user and their manager' do
+      User.import_users [user_with_manager, manager]
+
+      user = User.find_by_email_address 'user@example.com'
+      manager = User.find_by_email_address 'manager@example.com'
+      expect(user).not_to be_nil
+      expect(manager).not_to be_nil
+      expect(user.parent_id).to eq manager.id
     end
   end
 end
