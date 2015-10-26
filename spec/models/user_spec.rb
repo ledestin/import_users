a7ac95506daa0495d2e26619b1ee66857b9c76ed
Dmitry Maksyoma <ledestin@gmail.com>
2015-10-26 23:15:26 +1300

    Add User model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,23 @@ require 'rails_helper'
 
 describe User do
   let(:user_without_manager) { ['a@example.com', 'John', 'Doe'] }
+  let(:user_with_manager) do
+    ['a@example.com', 'John', 'Doe', 'manager@example.com']
+  end
 
   describe '.import_user' do
     it 'imports user that has no manager' do
       User.import_user user_without_manager
+
+      user = User.find_by_email_address('a@example.com')
+      expect(user.email_address).to eq 'a@example.com'
+      expect(user.first_name).to eq 'John'
+      expect(user.last_name).to eq 'Doe'
+      expect(user.parent_id).to be_nil
+    end
+
+    it 'imports user that has manager' do
+      User.import_user user_with_manager
 
       user = User.find_by_email_address('a@example.com')
       expect(user.email_address).to eq 'a@example.com'
